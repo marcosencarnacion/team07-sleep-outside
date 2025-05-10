46c40539d7e2672fdf4cdc8135d8ae527da22529
adlermo <adlerorteney@gmail.com>
2025-05-09 23:39:15 -0300

    Ran npm run format with new prettierrc rules

--- src/js/product.js
+++ src/js/product.js
@@ -1,28 +1,26 @@
-import { setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { setLocalStorage } from './utils.mjs'
+import ProductData from './ProductData.mjs'
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData('tents')
 
 function addProductToCart(product) {
   // check to see if theres anything in localstorage
   // if theres nothing, add new 'empty array'
   if (localStorage.getItem('so-cart') === null) {
-    localStorage.setItem('so-cart', '[]');
+    localStorage.setItem('so-cart', '[]')
   }
   // get empty array or existing array and push product to array
-  const productArray = JSON.parse(localStorage.getItem('so-cart'));
-  productArray.push(product);
+  const productArray = JSON.parse(localStorage.getItem('so-cart'))
+  productArray.push(product)
 
   // add to updated array to localstorage
-  setLocalStorage('so-cart', productArray);
+  setLocalStorage('so-cart', productArray)
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
+  const product = await dataSource.findProductById(e.target.dataset.id)
+  addProductToCart(product)
 }
 
 // add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+document.getElementById('addToCart').addEventListener('click', addToCartHandler)

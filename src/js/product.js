aa55e1a62629bb302257d15b7cd51c089165a758
mkanderson2024 <kalani2014.ma@gmail.com>
2025-05-14 19:45:06 -0600

    Completing Team Activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,22 @@
-import { getLocalStorage, qs, setClick, setLocalStorage } from './utils.mjs'
+import {
+  getLocalStorage,
+  qs,
+  setClick,
+  setLocalStorage,
+  getParam,
+} from './utils.mjs'
 import ProductData from './ProductData.mjs'
+import ProductDetails from './ProductDetails.mjs'
 
 const dataSource = new ProductData('tents')
+const productId = getParam('product')
+
+console.log(dataSource.findProductById(productId))
 
 const productArray = getLocalStorage('so-cart') || []
+
+const product = new ProductDetails(productID, dataSource)
+product.init()
 
 function updateCartCount() {
   // adding visual feedback

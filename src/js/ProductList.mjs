f9fbf58acd8e80c72adf8fd2457ba39a05f1fd4c
marcosencarnacion <enc24001@byui.edu>
2025-05-17 08:58:05 -0500

    updated

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,30 @@
+// ProductList.mjs
+import { renderListWithTemplate } from './utils.mjs';
+
+function productCardTemplate(product) {
+    return `
+    <li class="product-card">
+      <a href="/product_pages/index.html?product=${product.Id}">
+        <img src="${product.Images.PrimaryMedium}" alt="${product.Name}">
+        <h2>${product.Name}</h2>
+        <p>${product.ListPrice}</p>
+      </a>
+    </li>`;
+}
+
+export default class ProductList {
+    constructor(category, dataSource, listElement) {
+        this.category = category;
+        this.dataSource = dataSource;
+        this.listElement = listElement;
+    }
+
+    async init() {
+        const list = await this.dataSource.getData();
+        this.renderList(list);
+    }
+
+    renderList(list) {
+        renderListWithTemplate(productCardTemplate, this.listElement, list);
+    }
+}
